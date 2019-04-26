688ba6120532090ed5b3f8cf8fc94fe3b9a7b805
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-04-26 13:57:10 -0300

    Update CreepyBot.py

--- CreepyBot.py
+++ CreepyBot.py
@@ -8,7 +8,7 @@ import os
 Client = discord.Client()
 client = commands.Bot(command_prefix = "$")
 
-@bot.event
+@client.event
 async def on_ready():
     print("Thank you for turning me up!")
     await bot.change_presence(game=discord.Game(name='Currently on ' + str(len(bot.servers)) +
@@ -16,14 +16,14 @@ async def on_ready():
 
 
 
-@bot.event
+@client.event
 async def on_message(message):
     if message.content.startswith("hello"):
         msg = "Hello! (0.author.mention) How are you doing today?".format(message)
-        await bot.send_message(message.channel, msg)
+        await client.send_message(message.channel, msg)
     if message.content.startswith("bye"):
         msg = "Bye! (0.author.mention) i will see you later! (i hope)".format(message)
-        await bot.send_message(message.channel, msg)
+        await client.send_message(message.channel, msg)
 
-bot.run(os.getenv("BOT_TOKEN"))
+client.run(os.getenv("BOT_TOKEN"))
     
